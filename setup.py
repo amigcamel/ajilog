c39283ef248809567fdf16f999d812318c3d8166
Aji (阿吉) <amigcamel@gmail.com>
2018-01-24 15:10:13 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Setup."""
+from setuptools import setup, find_packages
+
+external_modules = [
+    'colorlog==3.1.0',
+]
+
+if __name__ == '__main__':
+    setup(
+        name='ajilog',
+        version='0.0.0',
+        packages=find_packages(),
+        install_requires=external_modules,
+        description='Wrapper for colorlog for better use.',
+    )
