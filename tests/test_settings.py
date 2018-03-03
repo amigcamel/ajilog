34c22c76263ef39b10b9ef152bf1facb4048fb84
amigcamel <amigcamel@gmail.com>
2018-03-04 00:43:55 +0800

    remove monkeypatch

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,24 +1,13 @@
 """Test settings."""
-from uuid import uuid4
-from os.path import isdir, join
-from tempfile import gettempdir
+from os.path import isdir
 from shutil import rmtree
-
-from six.moves import reload_module
 
 from .conf import import_ajilog
 
 
-def test_settings(monkeypatch):
+def test_settings():
     """Test rotate-related settings."""
     ajilog = import_ajilog()
-
-    reload_module(ajilog.settings)
-
-    monkeypatch.setitem(ajilog.settings.config['rotate'], 'enable', 'true')
-    monkeypatch.setitem(ajilog.settings.config['rotate'],
-                        'dir', join(gettempdir(), uuid4().hex))
-    monkeypatch.setitem(ajilog.settings.config['rotate'], 'level', 'debug')
 
     ajilog.logger.debug(u'')
     assert isdir(ajilog.settings.ROTATE_DIR)
