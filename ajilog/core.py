396d260d8d069229b71a83e5664f95dedb65b974
amigcamel <amigcamel@gmail.com>
2019-03-05 01:41:11 +0800

    initialize FileHandler and TimedRotatingHandler

--- ajilog/core.py
+++ ajilog/core.py
@@ -1,5 +1,6 @@
 """Logging configurations."""
 import logging
+import logging.handlers
 import sys
 
 from colorlog import ColoredFormatter
@@ -37,10 +38,30 @@ class initialize:
 
     def __init__(self):
         """Patch root loggers."""
+        # stream handler
         sh = logging.StreamHandler()
-        sh.setFormatter(ColoredFormatter(settings.LOG_FORMAT,
-                                         datefmt=settings.DATE_FORMAT,
-                                         log_colors=settings.LOG_COLORS))
+        sh.setFormatter(ColoredFormatter(
+            settings.STREAM['format'],
+            datefmt=settings.STREAM['datefmt'],
+            log_colors=settings.STREAM['log_colors']))
         logging.root.setLevel(logging.DEBUG)
         logging.root.addHandler(sh)
+
+        # file handler
+        fh = logging.FileHandler(settings.FILE['path'])
+        fh.setLevel(getattr(logging, settings.FILE['level']))
+        fh.setFormatter(logging.Formatter(
+            settings.FILE['format'], settings.FILE['datefmt']))
+        logging.root.addHandler(fh)
+
+        # rotating file handler
+        trh = logging.handlers.TimedRotatingFileHandler(
+            settings.TIME_ROTATE['path'],
+            when=settings.TIME_ROTATE['when'],
+            backupCount=settings.TIME_ROTATE['backupCount'])
+        trh.setLevel(getattr(logging, settings.TIME_ROTATE['level']))
+        trh.setFormatter(logging.Formatter(
+            settings.TIME_ROTATE['format'], settings.TIME_ROTATE['datefmt']))
+        logging.root.addHandler(trh)
+
         logging.debug('ajilog initialized')
--- ajilog/settings.py
+++ ajilog/settings.py
@@ -21,12 +21,30 @@ if ROTATE_ENABLE and (not exists(ROTATE_DIR)):
     print('rotating dir created: %s' % ROTATE_DIR)
 
 
-DATE_FORMAT = '%y-%m-%d %H:%M:%S'
-LOG_FORMAT = '%(log_color)s%(levelname)-8s%(reset)s %(yellow)s[%(asctime)s]%(reset)s%(white)s %(name)s %(funcName)s %(bold_purple)s:%(lineno)3d%(reset)s %(log_color)s%(message)s%(reset)s'  # noqa: E501
-LOG_COLORS = {
-    'DEBUG': 'blue',
-    'INFO': 'bold_cyan',
-    'WARNING': 'red,',
-    'ERROR': 'bg_bold_red',
-    'CRITICAL': 'red,bg_white',
+STREAM = {
+    'datefmt': '%y-%m-%d %H:%M:%S',
+    'format': '%(log_color)s%(levelname)-8s%(reset)s %(yellow)s[%(asctime)s]%(reset)s%(white)s %(name)s %(funcName)s %(bold_purple)s:%(lineno)3d%(reset)s %(log_color)s%(message)s%(reset)s',  # noqa: E501
+    'log_colors': {
+        'DEBUG': 'blue',
+        'INFO': 'bold_cyan',
+        'WARNING': 'red,',
+        'ERROR': 'bg_bold_red',
+        'CRITICAL': 'red,bg_white',
+    }
+}
+
+FILE = {
+    'datefmt': '%y-%m-%d %H:%M:%S',
+    'format': '%(levelname)-5s [%(asctime)s] %(name)s %(funcName)s :%(lineno)d %(message)s',  # noqa: E501
+    'level': 'INFO',
+    'path': join(gettempdir(), 'ajilog.log'),
+}
+
+TIME_ROTATE = {
+    'backupCount': 7,
+    'datefmt': '%y-%m-%d %H:%M:%S',
+    'format': '%(levelname)-5s [%(asctime)s] %(name)s %(funcName)s :%(lineno)d %(message)s',  # noqa: E501
+    'level': 'INFO',
+    'path': join(gettempdir(), 'ajilog_trh.log'),  # TODO: ensure dir exist
+    'when': 'midnight',
 }
